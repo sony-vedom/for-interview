268edd9c4d7e00d2867e21c24371e5fdabba303f
sony-vedom <minerva.lankmiller@gmail.com>
2024-08-19 16:07:20 +0500

    update client.ts

--- src/shared/api/client.ts
+++ src/shared/api/client.ts
@@ -5,7 +5,8 @@ import { ROUTES } from 'shared/config/routes'
 
 export const apiInstance = axios.create({
     headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        "Access-Control-Allow-Origin": "http://192.168.1.101:3150",
     },
     withCredentials: true,
     baseURL: API_URL,
