19149d03144e3d465b9c134628913ca5b88161a9
sony-vedom <minerva.lankmiller@gmail.com>
2024-08-19 15:58:12 +0500

    add proxy

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,20 @@ const config = {
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) =>
     mode === 'production'
-        ? config
+        ? {
+            ...config,
+            server: {
+                proxy: {
+                    '/api': {
+                        target: 'http://192.168.1.101:2500',
+                        changeOrigin: true,
+                        secure: false,
+                        rewrite: (p) => p.replace(/^\/api/, 'api')
+                    }
+                },
+                cors: false
+            }
+        }
         : {
             ...config,
             plugins: [
