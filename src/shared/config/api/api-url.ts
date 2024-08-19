6ddcd56b7e510726680c58b4272ce9d8eb1cea31
sony-vedom <minerva.lankmiller@gmail.com>
2024-08-19 16:10:40 +0500

    update api url

--- src/shared/config/api/api-url.ts
+++ src/shared/config/api/api-url.ts
@@ -1,1 +1,2 @@
-export const API_URL = import.meta.env.PROD ? `${import.meta.env.VITE_API_URL}/api` : "/api"
+// export const API_URL = import.meta.env.PROD ? `${import.meta.env.VITE_API_URL}/api` : "/api"
+export const API_URL = "/api"
