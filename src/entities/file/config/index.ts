240b9016fad62e85e3a4c3625ac6baacb1e37102
sony-vedom <minerva.lankmiller@gmail.com>
2024-09-02 11:44:29 +0500

    fix error

--- src/entities/file/config/index.ts
+++
@@ -1,5 +0,0 @@
-import { BASE_FILE_URLS, idNames } from 'entities/file/model/types'
-
-export const configAppFiles: Record<BASE_FILE_URLS, idNames> = {
-    [BASE_FILE_URLS.ANNUAL_MEDICAL_EXAMINATION]: 'medical_id'
-}
